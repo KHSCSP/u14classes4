f142f5dbe2b12e3254ae6d57fbd67c0529862ce5
heyscottn <heyscottn@gmail.com>
2024-03-21 13:18:27 -0500

    mm

--- apply.py
+++ apply.py
@@ -1,29 +1,3 @@
-# TODO
+# TODO (nothing here)
 # use your class from assignment #3
 # add some keyboard, mouse, collision
-
-import pygame, sys, random
-from pygame.locals import QUIT
-from bouncy_class import Bouncy
-
-pygame.init()
-screen = pygame.display.set_mode((800, 600))
-pygame.display.set_caption('Hello World!')
-
-
-
-
-while True:
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-    # -------- end event loop
-    screen.fill((255,255,255))        
-    
-
-
-
-
-    pygame.display.update()
-    pygame.time.delay(20)
